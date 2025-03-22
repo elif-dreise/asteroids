bd6baf484135010c893b4c19dd8e1fa967bffa9d
Elif Dreise <elifdreise@gmail.com>
2025-03-22 23:12:08 +0100

    Draw Black Screen

--- main.py
+++ main.py
@@ -2,9 +2,20 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
     print("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
 
 
 
